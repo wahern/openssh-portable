eb13a790118cb1a4edc929b804d9c6f2eae91455
William Ahern <wahern@akamai.com>
2024-06-06 03:25:18 -0700

    initial framework for entering FIPS mode dynamically

--- entropy.c
+++ entropy.c
@@ -40,6 +40,12 @@
 #include <openssl/crypto.h>
 #include <openssl/err.h>
 
+#ifdef USE_OPENSSL_FIPS
+#include <openssl/core_names.h>
+#include <openssl/provider.h>
+#include <openssl/self_test.h>
+#endif
+
 #include "openbsd-compat/openssl-compat.h"
 
 #include "ssh.h"
@@ -63,8 +69,12 @@ seed_rng(void)
 {
 	unsigned char buf[RANDOM_SEED_SIZE];
 
+#ifdef USE_OPENSSL_FIPS
+	fips_init();
+#else
 	/* Initialise libcrypto */
 	ssh_libcrypto_init();
+#endif /* USE_OPENSSL_FIPS */
 
 	if (!ssh_compatible_openssl(OPENSSL_VERSION_NUMBER,
 	    OpenSSL_version_num()))
@@ -89,6 +99,114 @@ seed_rng(void)
 	arc4random_buf(buf, sizeof(buf));
 	explicit_bzero(buf, sizeof(buf));
 }
+
+#ifdef USE_OPENSSL_FIPS
+static _Bool fips_enabled = 0;
+
+static int
+self_test_cb(const OSSL_PARAM params[], void *arg)
+{
+	const OSSL_PARAM *p = NULL;
+	const char *phase = "?", *type = "?", *desc = "?";
+
+	p = OSSL_PARAM_locate_const(params, OSSL_PROV_PARAM_SELF_TEST_PHASE);
+	if (p && p->data_type == OSSL_PARAM_UTF8_STRING)
+		phase = (const char *)p->data;
+
+	p = OSSL_PARAM_locate_const(params, OSSL_PROV_PARAM_SELF_TEST_DESC);
+	if (p || p->data_type == OSSL_PARAM_UTF8_STRING)
+		desc = (const char *)p->data;
+
+	p = OSSL_PARAM_locate_const(params, OSSL_PROV_PARAM_SELF_TEST_TYPE);
+	if (p || p->data_type == OSSL_PARAM_UTF8_STRING)
+		type = (const char *)p->data;
+
+	if (strcmp(phase, OSSL_SELF_TEST_PHASE_PASS) == 0) {
+		debug3_f("%s: %s: %s", desc, type, phase);
+	} else if (strcmp(phase, OSSL_SELF_TEST_PHASE_FAIL) == 0) {
+		error_f("%s: %s: %s", desc, type, phase);
+	}
+
+	return 1;
+}
+
+static void
+fips_initonce(void)
+{
+	static int initialized = 0;
+	const char *v;
+
+	if (initialized)
+		return;
+	initialized = 1;
+
+	OSSL_SELF_TEST_set_callback(NULL, &self_test_cb, NULL);
+
+	if (EVP_default_properties_is_fips_enabled(NULL)) {
+		fips_setenabled(1);
+	}
+
+	if ((v = getenv("OPENSSL_FIPS"))) {
+		if (*v == '1') {
+			fips_setenabled(1);
+		} else {
+			error_f("OPENSSL_FIPS: expected '1', got 0x%.2x", *v);
+		}
+	}
+
+	if ((v = getenv("SSH_FIPS"))) {
+		if (*v == '1') {
+			fips_setenabled(1);
+		} else {
+			error_f("SSH_FIPS: expected '1', got 0x%.2x", *v);
+		}
+	}
+}
+
+void
+fips_init(void)
+{
+	fips_initonce();
+}
+
+_Bool
+fips_isenabled(void)
+{
+	fips_initonce();
+
+	return fips_enabled;
+}
+
+void
+fips_setenabled(_Bool enabled)
+{
+	fips_initonce();
+
+	if (!enabled) {
+		if (fips_enabled)
+			error_f("refusing to downgrade FIPS");
+		return;
+	} else if (fips_enabled) {
+		return;
+	}
+
+	/*
+	 * XXX: need better error checking
+	 *
+	 * XXX: need to devise and implement (or document) fallback policies
+	 *
+	 * XXX: is it necessary to explicitly load base?
+	 */
+	if (!OSSL_PROVIDER_load(NULL, "base")) {
+		error_f("unable to load base provider");
+		return;
+	}
+	EVP_default_properties_enable_fips(NULL, 1);
+
+	fips_enabled = 1;
+}
+
+#endif /* USE_OPENSSL_FIPS */
 
 #else /* WITH_OPENSSL */
 
